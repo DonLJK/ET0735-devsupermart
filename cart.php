04aaee4a9f302d3d00aec8eaa6c48ca7786bd8d5
FaithDeVera <faithkirstendv14.21@ichat.sp.edu.sg>
2023-01-24 00:51:39 +0800

    24 jan push - faith

--- cart.php
+++ cart.php
@@ -7,7 +7,39 @@ if($conn->connect_error)
     die("Connection failed: ".$conn->connect_error);
 }
 
+//HAD TO CHANGE THE PRODCT TABLE COLUMN ID TO PROD_ID
+if (isset($_POST['purchase'])) {
+    //ADD MORE POSTS
+    $sql3 = "SELECT user.id, product.prod_id, FROM user, product where user.id='1' AND product.prod_id='2'";	
 
+
+$r = @mysqli_query($conn, $sql3);
+if($r == true)
+{
+    $row = mysqli_fetch_assoc($r);
+    echo $row['id'];
+    echo $row['prod_id'];
+    $uid = $row['id'];
+    $prod = $row['prod_id'];
+    $sql2 = "INSERT INTO cart (id, customer_id, prod_id, prod_quantity) VALUES ('0','$uid', '$prod', '4')";
+    $q = @mysqli_query($conn, $sql2);
+    if($q == true)
+    {
+        echo "YOU DID IT";
+    }
+    else
+    {
+        echo "Error: ".$q."<br>".$conn->error;
+    }
+}
+else
+{
+    echo "Error: ".$r."<br>".$conn->error;
+}
+
+}
+
+$conn->close();
 
 
 
